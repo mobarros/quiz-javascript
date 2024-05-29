ff7c4b2a7e90ad9b619e18afdb013ddbe1356cc9
Monique Barros <99218248+mobarros@users.noreply.github.com>
2024-05-29 17:09:00 -0300

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,119 @@
+var myQuestions = [
+	{
+		question: "Qual a Capital do Brasil",
+		answers: {
+			a: 'São Paulo',
+			b: 'Brasília',
+			c: 'Rio de Janeiro'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "Qual é o maior numero?",
+		answers: {
+			a: '-30',
+			b: '5',
+			c: '10',
+			d: '9'
+		},
+		correctAnswer: 'c'
+	},
+	{
+		question: "Qual o maior País do Mundo?",
+		answers: {
+			a: 'Rússia',
+			b: 'China',
+			c: 'Estados Unidos',
+			
+		},
+		correctAnswer: 'a'
+	}
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+	function showQuestions(questions, quizContainer){
+		
+		var output = [];
+		var answers; 
+
+		
+		for(var i=0; i<questions.length; i++){
+			
+			
+			answers = [];
+
+			
+			for(letter in questions[i].answers){
+
+				
+				answers.push(
+					'<label>'
+						+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+					
+						+ questions[i].answers[letter]
+					+ '</label>'
+				);
+			}
+
+			
+			output.push(
+				'<div class="question">' + questions[i].question + '</div>'
+				+ '<div class="answers">' + answers.join('') + '</div>'
+			);
+		}
+
+		
+		quizContainer.innerHTML = output.join('');
+	}
+
+
+	function showResults(questions, quizContainer, resultsContainer){
+			
+		
+		var answerContainers = quizContainer.querySelectorAll('.answers');
+		
+		
+		var userAnswer = '';
+		var numCorrect = 0;
+		
+		
+		for(var i=0; i<questions.length; i++){
+
+			
+			userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+			
+			
+			
+			if(userAnswer===questions[i].correctAnswer){
+				
+				numCorrect++;
+				
+				
+				answerContainers[i].style.color = 'lightgreen';
+			}
+			
+			else{
+				
+				answerContainers[i].style.color = 'red';
+			}
+		}
+
+			
+			resultsContainer.innerHTML = numCorrect + ' de ' + questions.length;
+		}
+
+	
+	showQuestions(questions, quizContainer);
+
+	
+	submitButton.onclick = function(){
+		showResults(questions, quizContainer, resultsContainer);
+	}
+}
